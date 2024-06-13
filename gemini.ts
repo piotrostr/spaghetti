4d9f053d43cfc773ccbed3024e9ddcbe82a5da92
Piotr Ostrowski <piotrostr@google.com>
2024-06-13 14:33:08 +0200

    generate first recipe from *real* raw recipe

--- gemini.ts
+++ gemini.ts
@@ -14,37 +14,62 @@ export class Gemini {
     });
   }
 
-  async makeRecipe(prompt: string): Promise<Recipe> {
+  async makeRecipe(rawRecipe: string): Promise<Recipe> {
     const response = await this.model.generateContent(
-      prompt +
-        "\n" +
-        ` the response should be in JSON format, matching the following interface:
-		 export interface Recipe {
-			description?: string;
-			ingredients: Array<Ingredient>;
-			instructions: Array<Instruction>;
-		}
-		
-		export interface Ingredient {
-			name: string;
-			quantity: string;
-			unit: string;
-		}
-		
-		export type Instruction = string;
-		export type ID = string;
+      `
+			jestem szefem kuchni. chce utworzyc szybkie przepisy z przeliczaniem
+			jednostek. potrzebuje precyzyjnej informacji z podzialem na ilosc porcji i
+			osob.
 
-		return only the JSON so it can be instantly parsed into an object
-		 `
+			Na podstawie przepisu, wygeneruj reprezentacje tego opisu w formacie JSON
+			jesli podajesz informacje o winie zamień szklankę na kieliszek i przelicz jednostki.
+
+			przeliczaj według poniższych zasad:
+
+			kieliszek mocnego alkoholu ≈ 0,045 – 0,05 litra 
+			Kieliszek wina ≈ 0,1 - 0,125 litra
+			filiżanka ≈ 240 cm³[1] lub 250 cm³[a][2] (ang. cup)
+			łyżka stołowa ≈ 15 cm³[1] (z wyjątkiem Australii, gdzie jest to ok. 20 cm³[3])
+			łyżeczka ≈ 5 cm³
+			szczypta ≈ 1/4 – 1/2 grama
+			kilka gałązek ≈ doniczka
+			jesli podajesz informacje o alkoholu mocnym zamień szklankę na kieliszek i przelicz odpowiednio jednostki.
+
+			Oto przepis ze strony:
+			${rawRecipe}
+
+      Odpowiedz podaj w formacie JSON zgodnym z poniższym schematem:
+
+			export interface Recipe {
+				description?: string;
+				ingredients: Array<Ingredient>;
+				instructions: Array<Instruction>;
+			}
+		
+			export interface Ingredient {
+				name: string;
+				quantity: string;
+				unit: string;
+			}
+
+			export type Instruction = string;
+			export type ID = string;
+			
+			zwracaj tylko JSON, aby można go było natychmiast przetworzyć na obiekt,
+			odpowiedz powinna zawierac tylko poprawny ciąg JSON
+			`
     );
+    console.debug(`Response: ${JSON.stringify(response)}`);
     // TODO hacky, should use a JSON validator and handle errors if model
     // does not return a valid response, should not be hard-indexing
+    console.debug(response.response.candidates);
     const text = response.response.candidates?.map(
       (c) => c.content.parts[0]
     )[0]["text"];
+    console.debug(text);
     if (!text) {
       throw new Error("Invalid response from model");
     }
-    return JSON.parse(text) as Recipe;
+    return JSON.parse(text.replace("```json", "").replace("```", "")) as Recipe;
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,54 @@
 import { Gemini } from "../gemini";
 import { searchProduct } from "./search";
 
+// TODOs
+// * it might be a good idea to narrow down categories to food not to get cleaning
+// products n stuff
+// * it might make sense to ignore salt, pepper, sugar, water, etc. as they are
+// usually present in every recipe and available in virtually every household
 (async () => {
   try {
-    const result = await searchProduct("banan", 1, 0);
     const gemini = new Gemini("vertex-ai-playground-402513", "europe-central2");
-    const recipe = await gemini.makeRecipe("I want to make a banana cake");
+    const rawRecipe = `
+    SKŁADNIKI
+    1 - 2 PORCJE
+    2 pomidory
+    1/2 łyżki masła lub oliwy
+    1/2 ząbka czosnku
+    przyprawy: sól i świeżo zmielony pieprz, szczypta oregano oraz opcjonalnie chili i kminu rzysmkiego
+    2 jajka
+    do podania: świeża bazylia, bagietka
+
+    PRZYGOTOWANIE
+    Przygotować pomidory: sparzyć, obrać ze skórki, pokroić na ćwiartki, wykroić
+    szypułki, miąższ pokroić w kosteczkę.  Na niedużą patelnię (około 20 cm
+    średnicy) włożyć masło lub wlać oliwę oraz starty czosnek, chwilę podsmażyć.
+    Pomidory włożyć na patelnię, doprawić solą, pieprzem i przyprawami. Wymieszać
+    i intensywnie smażyć na większym ogniu przez około 4 minuty, już bez
+    mieszania (wówczas pomidory odparują i zachowają swoją strukturę, jeśli
+    będziemy mieszać zrobi się przecier).  Do podsmażonych pomidorów wbić jajka,
+    doprawić solą. Przykryć i gotować przez około 3 minuty lub do czasu aż białka
+    jajek będą ścięte. Podawać ze świeżą bazylią i bagietką.      
+    `;
+    const recipe = await gemini.makeRecipe(rawRecipe);
+
+    const productResultsPromises = recipe.ingredients.map((ingredient) =>
+      searchProduct(ingredient.name, 5, 0)
+    );
+
+    const results = await Promise.all(productResultsPromises);
+    const postprocessedResults = results.map((result) =>
+      result["data"]["productSearch"]["results"].map((r) => ({
+        id: r["id"],
+        sku: r["sku"],
+        name: r["name"],
+        price: r["price"].value,
+        slug: r["slug"],
+      }))
+    );
     console.log(recipe);
-    console.log(result["data"]["productSearch"]["results"]);
+    console.log(postprocessedResults);
   } catch (error) {
-    console.error("Error fetching products:", error);
+    console.error("Error", error);
   }
 })();
